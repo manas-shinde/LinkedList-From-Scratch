f991df7d655e5b89afa5bfb76194c821e7eaef39
manas-shinde <manas.a.shinde11@gmail.com>
2025-07-25 18:24:50 +0530

    feat: Singly Linked List implementation from scratch.

---
+++ linked_list.py
@@ -0,0 +1,65 @@
+from node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_node(self, value: int):
+        new_node: Node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+        elif value >= self.head.value:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            previous: Node = self.head
+            runner: Node = self.head.next
+
+            while runner is not None and value < runner.value:
+                previous = runner
+                runner = runner.next
+
+            new_node.next = runner
+            previous.next = new_node
+
+    def __str__(self):
+        """
+        Returns a string representation of the linked list.
+        e.g., "1 -> 2 -> 3 -> None"
+        """
+        result: str = ""
+        runner_node: Node = self.head
+
+        # Handle the case of an empty list
+        if runner_node is None:
+            return "None"
+
+        # Traverse the list and build the string
+        while runner_node is not None:
+            result += str(runner_node.value)
+            if runner_node.next is not None:
+                result += " -> "
+            runner_node = runner_node.next
+
+        return result
+
+    def __len__(self) -> int:
+        cnt: int = 0
+        runner: Node = self.head
+
+        while runner is not None:
+            cnt += 1
+            runner = runner.next
+
+        return cnt
+
+    def count_node(self) -> int:
+        return self.count_node_recursive(self.head)
+
+    def count_node_recursive(self, node) -> int:
+        if node is None:
+            return 0
+
+        return 1 + self.count_node_recursive(node.next)
